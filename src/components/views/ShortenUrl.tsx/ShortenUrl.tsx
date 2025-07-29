ab09d9819a587d8f1ec24578f6fb23b7f1572622
haqi <hqq.amrullah@gmail.com>
2025-07-29 18:26:08 +0700

    feat: Generate QRCode

--- src/components/views/ShortenUrl.tsx/ShortenUrl.tsx
+++ src/components/views/ShortenUrl.tsx/ShortenUrl.tsx
@@ -2,14 +2,18 @@ import { Button, Card, CardBody, Input, Spinner } from '@heroui/react';
 import Image from 'next/image';
 import useShortenUrl from './useShortenUrl';
 import { Controller } from 'react-hook-form';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FaCopy } from 'react-icons/fa';
+import QRCode from 'react-qr-code';
+import {downloadQR} from '@/libs/downloadQR';
 
 const ShortenUrl = () => {
   const [shortenedUrl, setShortenedUrl] = useState('');
   const { control, handleSubmit, handleSubmitUrl, isPendingShorten } = useShortenUrl({
     setShortenedUrl,
   });
+  const qrRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="flex w-full flex-col items-center justify-center gap-10 lg:flex-row lg:gap-20">
       <div className="flex w-1/3 flex-col items-center justify-center gap-10">
@@ -56,6 +60,26 @@ const ShortenUrl = () => {
               <FaCopy size={18} />
               Copy
             </Button>
+            {shortenedUrl && (
+              <div 
+                ref={qrRef} 
+                className='flex flex-col items-center p-4 rounded'>
+                  <p className='text-sm'>
+                    <QRCode value={shortenedUrl} size={128}></QRCode>
+                  </p>
+              </div>
+            )}
+            <Button 
+              disabled={!shortenedUrl}  
+              onPress={() => downloadQR(qrRef, 'shortened-url.png')}
+              className={`flex items-center gap-2 rounded-md px-4 py-2 transition-colors duration-200 ${
+                shortenedUrl
+                  ? 'bg-blue-500 text-white hover:bg-blue-600'
+                  : 'cursor-not-allowed bg-gray-300 text-gray-500'
+              }`}>
+                Download QR
+            </Button>
+            
           </form>
         </CardBody>
       </Card>
---
+++ src/libs/downloadQR.ts
@@ -0,0 +1,20 @@
+import {toPng} from 'html-to-image';
+
+export const downloadQR =
+    async (ref: React.RefObject<HTMLElement>, filename = 'qrcode.png') => {
+        if (!ref.current) return;
+
+        try {
+            const  dataUrl = await toPng(ref.current, {
+                cacheBust: true,
+                backgroundColor: '#ffffff',
+                pixelRatio: 2
+            });
+            const link = document.createElement('a');
+            link.href = dataUrl;
+            link.download = filename;
+            link.click();
+        } catch (error) {
+            console.error('Failed to download QR Code', error);
+        }
+    };
